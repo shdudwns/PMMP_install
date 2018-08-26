dbd0366ab3d441becb1d99a2acfd9467420d0c20
jun04 <jun040329@gmail.com>
2018-08-26 15:02:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ wget https://jenkins.pmmp.io/job/PocketMine-MP/lastSuccessfulBuild/artifact/Pock
 echo "PHP바이너리 설치중.....";
 wget https://jenkins.pmmp.io/job/PHP-7.2-Linux-x86_64/lastSuccessfulBuild/artifact/PHP_Linux-x86_64.tar.gz;
 tar -xzvf PHP_Linux-x86_64.tar.gz;
+ehco "start.sh 설치중";
+wget https://raw.githubusercontent.com/pmmp/PocketMine-MP/master/start.sh;
+chmod 777 start.sh
+echo "설치완료!!";
