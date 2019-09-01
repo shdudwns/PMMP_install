5b3f6a7704cccac603283263a4bc1b32d5a25b0e
jun04 <jun040329@gmail.com>
2019-09-02 00:06:11 +0900

    Update link

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "PocketMine-MP 설치중......";
 wget https://jenkins.pmmp.io/job/PocketMine-MP/lastSuccessfulBuild/artifact/PocketMine-MP.phar;
 echo "PHP바이너리 설치중.....";
-wget https://jenkins.pmmp.io/job/PHP-7.2-Linux-x86_64/lastSuccessfulBuild/artifact/PHP_Linux-x86_64.tar.gz;
+wget https://jenkins.pmmp.io/job/PHP-7.3-Linux-x86_64/lastSuccessfulBuild/artifact/PHP_Linux-x86_64.tar.gz;
 tar -xzvf PHP_Linux-x86_64.tar.gz;
 echo "start.sh 설치중";
 wget https://raw.githubusercontent.com/pmmp/PocketMine-MP/master/start.sh;
