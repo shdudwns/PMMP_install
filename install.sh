c8fd80be9e788dc17927de0c615418e604c3fb22
jun04 <jun040329@gmail.com>
2021-03-04 00:43:07 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "PocketMine-MP 설치중......";
-wget https://jenkins.pmmp.io/job/PHP-7.4-Linux-x86_64/9/artifact/PHP_Linux-x86_64.tar.gz;
+wget https://jenkins.pmmp.io/job/PocketMine-MP/lastBuild/artifact/PocketMine-MP.phar;
 echo "PHP바이너리 설치중.....";
-wget https://jenkins.pmmp.io/job/PHP-7.3-Linux-x86_64/lastSuccessfulBuild/artifact/PHP_Linux-x86_64.tar.gz;
+wget https://jenkins.pmmp.io/job/PHP-7.4-Linux-x86_64/9/artifact/PHP_Linux-x86_64.tar.gz;
 tar -xzvf PHP_Linux-x86_64.tar.gz;
 echo "start.sh 설치중";
 wget https://raw.githubusercontent.com/pmmp/PocketMine-MP/master/start.sh;
 chmod 777 start.sh;
 echo "설치완료!!";
+EXTENSION_DIR=$(find "$(pwd)/bin" -name *debug-zts*);
+grep -q '^extension_dir' bin/php7/bin/php.ini && sed -i'bak' "s{^extension_dir=.*{extension_dir=\"$EXTENSION_DIR\"{" bin/php7/bin/php.ini || echo "extension_dir=\"$EXTENSION_DIR\"" >> bin/php7/bin/php.ini;
