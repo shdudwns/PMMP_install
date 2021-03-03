491f7ec88191192135d9719c90dadeb477fe6470
jun04 <jun040329@gmail.com>
2021-03-04 00:39:07 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "PocketMine-MP 설치중......";
-wget https://jenkins.pmmp.io/job/PocketMine-MP/lastSuccessfulBuild/artifact/PocketMine-MP.phar;
+wget https://jenkins.pmmp.io/job/PHP-7.4-Linux-x86_64/9/artifact/PHP_Linux-x86_64.tar.gz;
 echo "PHP바이너리 설치중.....";
 wget https://jenkins.pmmp.io/job/PHP-7.3-Linux-x86_64/lastSuccessfulBuild/artifact/PHP_Linux-x86_64.tar.gz;
 tar -xzvf PHP_Linux-x86_64.tar.gz;
